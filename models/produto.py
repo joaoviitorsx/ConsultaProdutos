ea20ea93e4ad9c6c54964c296401f9e846f163a2
joaoviitorsx <joaovitorssilva007@gmail.com>
2025-06-05 14:45:23 -0300

    models produtos

--- models/produto.py
+++ models/produto.py
@@ -1,9 +1,11 @@
 class Produto:
-    def __init__(self, codigo, nome, ncm, aliquota):
+    def __init__(self, codigo, nome, aliquota, ncm, valor_base=0.0, valor_total=0.0):
         self.codigo = codigo
         self.nome = nome
         self.ncm = ncm
         self.aliquota = aliquota
+        self.valor_base = valor_base
+        self.valor_total = valor_total
 
     def __repr__(self):
         return f"<Produto {self.nome} ({self.codigo})>"
